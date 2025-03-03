a60de9bd49dc7ef70594e3dac49cb87bdb9cc3a5
Mrudul John Mathews <67854062+mruduljohn@users.noreply.github.com>
2025-03-03 16:55:48 +0530

    Redirect to Login

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,10 +4,14 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
   server: {
     historyApiFallback: true,
+  },
+  preview: {
+    historyApiFallback: true, // Also fix for Vite build preview mode
   },
 });
